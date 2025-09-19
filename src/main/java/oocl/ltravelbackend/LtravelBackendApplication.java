8338a25de16b789bb3ef52b7079f582524b818b3
Andy Hui <chhuiwork@gmail.com>
2025-09-19 19:07:28 +0800

    build: Configure application yaml files and docker files for CI/CD pipeline

---
+++ src/main/java/oocl/ltravelbackend/LtravelBackendApplication.java
@@ -0,0 +1,13 @@
+package oocl.ltravelbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LtravelBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LtravelBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/oocl/ltravelbackend/LtravelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package oocl.ltravelbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LtravelBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
