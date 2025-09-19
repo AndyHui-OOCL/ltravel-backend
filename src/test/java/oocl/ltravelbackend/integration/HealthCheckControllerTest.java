cb3fee1a767013bc4938cbc42fb96bba2c3f8079
Andy Hui <chhuiwork@gmail.com>
2025-09-19 21:04:56 +0800

    Refactor: Renamed file directory

--- src/test/java/oocl/ltravelbackend/integration/HealthCheckControllerTest.java
+++ src/test/java/oocl/ltravelbackend/integration/HealthCheckControllerTest.java
@@ -1,4 +1,4 @@
-package oocl.ltravelbackend.integrationtest;
+package oocl.ltravelbackend.integration;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
