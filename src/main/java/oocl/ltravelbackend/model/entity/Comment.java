4f8a20fa43aca837e5e86bee3297731eabb2d158
YUANJA3 <yuanja3@oocl.com>
2025-09-22 13:55:15 +0800

    feat:add entity Comment and OfficialComment

---
+++ src/main/java/oocl/ltravelbackend/model/entity/Comment.java
@@ -0,0 +1,23 @@
+package oocl.ltravelbackend.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "comment")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private Boolean isLike;
+    private Long travelComponentId;
+    private Long userId;
+}
