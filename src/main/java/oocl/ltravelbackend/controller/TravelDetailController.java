960cbc8a1222df3e717e3dd716139583f89ed55b
Gavin Zhang <ZHANGGA7@OOCL.COM>
2025-09-22 14:42:31 +0800

    refactor: rename

--- src/main/java/oocl/ltravelbackend/controller/TravelDetailController.java
+++ src/main/java/oocl/ltravelbackend/controller/TravelDetailController.java
@@ -1,24 +1,24 @@
 package oocl.ltravelbackend.controller;
 
-import oocl.ltravelbackend.model.entity.TravelComponent;
 import oocl.ltravelbackend.model.entity.TravelPlan;
-import oocl.ltravelbackend.service.TravelDatilService;
+import oocl.ltravelbackend.service.TravelDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/travelDetail")
-public class TravelDatilController {
+public class TravelDetailController {
 
     @Autowired
-    private TravelDatilService travelDatilService;
+    private TravelDetailService travelDetailService;
 
-//    @GetMapping
-//    public ResponseEntity<TravelPlan> getTravelPlans() {
-//
-//    }
+    @GetMapping("/{id}")
+    public ResponseEntity<TravelPlan> getTravelPlan(@PathVariable Long id) {
+        return ResponseEntity.ok(new TravelPlan());
+    }
 
 }
--- src/main/java/oocl/ltravelbackend/service/TravelDetailService.java
+++ src/main/java/oocl/ltravelbackend/service/TravelDetailService.java
@@ -3,5 +3,5 @@ package oocl.ltravelbackend.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public class TravelDatilService {
+public class TravelDetailService {
 }
