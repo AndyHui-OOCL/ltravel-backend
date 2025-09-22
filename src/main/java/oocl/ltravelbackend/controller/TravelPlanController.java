85de75cf83d21784a01d3d0d3e29b6b83cdf915e
Andy Hui <andy.hui@oocl.com>
2025-09-22 14:00:41 +0800

    Feat: Add travel plan controller

---
+++ src/main/java/oocl/ltravelbackend/controller/TravelPlanController.java
@@ -0,0 +1,8 @@
+package oocl.ltravelbackend.controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/travel-plans")
+public class TravelPlanController {
+}
