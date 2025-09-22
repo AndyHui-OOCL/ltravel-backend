81e9ac52916f5340fac1f15d00c8d646ed8fd926
YUANJA3 <yuanja3@oocl.com>
2025-09-22 15:01:41 +0800

    fix:add exception package.

--- src/main/java/oocl/ltravelbackend/controller/OfficialCommentController.java
+++ src/main/java/oocl/ltravelbackend/controller/OfficialCommentController.java
@@ -6,14 +6,15 @@ import oocl.ltravelbackend.service.OfficialCommentService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class OfficialCommentController {
     private final OfficialCommentService officialCommentService;
-    @GetMapping("/official-comment")
-    public OfficialComment getOfficialComment(Long travelPlanId) {
+    @GetMapping("/official-comment/{travelPlanId}")
+    public OfficialComment getOfficialComment(@PathVariable Long travelPlanId) {
         return officialCommentService.getOfficialCommentByTravelPlanId(travelPlanId).orElse(null);
     }
 }
