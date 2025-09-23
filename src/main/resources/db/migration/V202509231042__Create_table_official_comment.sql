0bc566c05a1f4555ebc1d05ae8e01f5c158af18b
YUANJA3 <yuanja3@oocl.com>
2025-09-23 10:56:34 +0800

    fix:the right folder for flyway sql is db.migration but not db.migrations

--- src/main/resources/db/migration/V202509231042__Create_table_official_comment.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `official_comment` (
-                                    `id` bigint NOT NULL AUTO_INCREMENT,
-                                    `event_comment` varchar(255) DEFAULT NULL,
-                                    `overall_comment` varchar(255) DEFAULT NULL,
-                                    `promote_reason` varchar(255) DEFAULT NULL,
-                                    `rating` double DEFAULT NULL,
-                                    `travel_plan_id` bigint DEFAULT NULL,
-                                    PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
